859c9e00240d792047e3656b000cc1d7ccf14470
Bram <bramrutgers14@gmail.com>
2022-11-22 17:39:06 +0100

    Created the basic

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
-function genDivs(amount){
-    let container = document.getElementById("container");
-    for(i = 0; i < amount; i++){
-        let row = document.createElement("div");
-    }
-}
+const container = document.getElementById("container");
+function makeRows(rows, cols) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    for (c = 0; c < (rows * cols); c++) {
+      let cell = document.createElement("div");
+      cell.setAttribute("style", "width = 1fr");
+      cell.setAttribute("style", "height = 1fr");
+      container.appendChild(cell).className = "grid-item";
+    };
+  };
+
+  makeRows(16, 16);
+
+let grids = document.querySelectorAll('.grid-item');
+let isDrawing;
+grids.forEach((div) => {
+    div.addEventListener('mousedown', () => {
+        isDrawing = true;
+    });
+    div.addEventListener('mousemove', () => {
+        if(isDrawing){
+            div.setAttribute("style", "background-color: black;")
+        }
+    });
+    div.addEventListener('mouseup', () => {
+        if(isDrawing){
+            isDrawing = false;
+        }
+    });
+  });
+
+const butamount = document.querySelector('#amount');
+butamount.addEventListener('click', () => {
+    let amt = prompt("Choose amount");
+    container.replaceChildren("");
+    makeRows(amt, amt);
+  });
