e5c0562a92ac9d5dde1af0a92880d10adcc03f17
Bram <bramrutgers14@gmail.com>
2022-11-24 00:24:53 +0100

    Finished the layout, reset button and change size

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const container = document.getElementById("container");
+let grids;
+
 function makeRows(rows, cols) {
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
@@ -8,31 +10,50 @@ function makeRows(rows, cols) {
       cell.setAttribute("style", "height = 1fr");
       container.appendChild(cell).className = "grid-item";
     };
+    grids = document.querySelectorAll('.grid-item');
   };
 
-  makeRows(16, 16);
+makeRows(8, 8);
 
-let grids = document.querySelectorAll('.grid-item');
+function draw(gridItem){
+  gridItem.setAttribute("style", "background-color: #001f3f;");
+};
+
 let isDrawing;
-grids.forEach((div) => {
-    div.addEventListener('mousedown', () => {
-        isDrawing = true;
+function manageDivs(){
+  grids.forEach((div) => {
+      div.addEventListener('mousedown', () => {
+          isDrawing = true;
+      });
+      div.addEventListener('mousemove', () => {
+          if(isDrawing){
+              draw(div);
+          }
+      });
+      div.addEventListener('mouseup', () => {
+          if(isDrawing){
+              isDrawing = false;
+          }
+      });
     });
-    div.addEventListener('mousemove', () => {
-        if(isDrawing){
-            div.setAttribute("style", "background-color: black;")
-        }
-    });
-    div.addEventListener('mouseup', () => {
-        if(isDrawing){
-            isDrawing = false;
-        }
-    });
+};
+
+manageDivs();
+
+const butAmount = document.querySelector('#amount');
+butAmount.addEventListener('click', () => {
+    let amt = prompt("Choose amount");
+    while (amt > 100 || amt == 100){
+      amt = prompt("The amount should be less than 100: ");
+    }
+    container.innerHTML = '';
+    makeRows(amt, amt);
+    manageDivs();
   });
 
-const butamount = document.querySelector('#amount');
-butamount.addEventListener('click', () => {
-    let amt = prompt("Choose amount");
-    container.replaceChildren("");
-    makeRows(amt, amt);
+const butReset = document.querySelector('#reset');
+butReset.addEventListener('click', () => {
+  grids.forEach((div) => {
+    div.setAttribute("style", "backgroound-color: white;");
   });
+});
